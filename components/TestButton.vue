094fc25e2af3bc54461a0bab1b59d7eca91008f6
xiongben <xiongben1993@gmail.com>
2021-01-11 15:52:40 +0800

    [add] component

---
+++ components/TestButton.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="box">
+    test button component box
+  </div>
+</template>
+<script>
+
+</script>
+<style scoped>
+  .box{
+    width: 100px;
+    height: 100px;
+    background: blue;
+  }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -7,16 +7,18 @@
       <li><nuxt-link :to="{name:'news',params:{newsId:3306}}">news</nuxt-link></li>
       <li><nuxt-link :to="{name:'list'}">List Page</nuxt-link></li>
     </ul>
+    <TestButton/>
   </div>
 </template>
 
 <script>
-
+import TestButton from "@/components/TestButton"
 
 export default {
   components: {
-
+    TestButton
   }
+
 }
 </script>
 
